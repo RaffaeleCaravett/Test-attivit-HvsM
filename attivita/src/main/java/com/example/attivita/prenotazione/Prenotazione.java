f33a4bdb1ee39c3028ca0a47f513b445fa7b9337
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-11 21:45:26 +0200

    creating related entities

---
+++ attivita/src/main/java/com/example/attivita/prenotazione/Prenotazione.java
@@ -0,0 +1,28 @@
+package com.example.attivita.prenotazione;
+
+import com.example.attivita.attivita.Attivita;
+import com.example.attivita.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "prenotazione")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Prenotazione {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "attivita_id")
+    private Attivita attivita;
+    private StatoPrenotazione statoPrenotazione;
+}
