c784f02da74ebef1f64e365ac8d0325ba695ee11
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-12 12:10:42 +0200

    created categoria repository

--- attivita/src/main/java/com/example/attivita/attivita/AttivitaService.java
+++ attivita/src/main/java/com/example/attivita/attivita/AttivitaService.java
@@ -1,7 +1,33 @@
 package com.example.attivita.attivita;
 
+import com.example.attivita.categoria.Categoria;
+import com.example.attivita.categoria.CategoriaRepository;
+import com.example.attivita.payloads.entities.AttivitaDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AttivitaService {
+    @Autowired
+    AttivitaRepository attivitaRepository;
+    @Autowired
+    CategoriaRepository categoriaRepository;
+
+    public Attivita save(AttivitaDTO attivitaDTO){
+        Attivita attivita = new Attivita();
+
+        attivita.setDate(LocalDate.of(attivitaDTO.anno(),attivitaDTO.mese(),attivitaDTO.giorno()));
+        attivita.setNome(attivitaDTO.nome());
+        attivita.setLuogo(attivitaDTO.luogo());
+        attivita.setDisponibilita(true);
+
+        List<Categoria>  categoriaList = new ArrayList<>();
+        for(long l : attivitaDTO.categorie_id()){
+            Categoria categoria = cat
+        }
+    }
 }
